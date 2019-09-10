aee83f54e4848db1be42833419d09f983a7da7e8
Aiden P <50061154+PrecurseAP@users.noreply.github.com>
2019-09-10 15:12:32 -0400

    main commit - base files + a line of code for declaring a motor that is unfinished
    kunk kunk im a oilyy boi

---
+++ F-Team_main/src/main.cpp
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\MasukVex 09                                      */
+/*    Created:      Tue Sep 10 2019                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+
+using namespace vex;
+
+vex::motor BackLeft = vex::motor()
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain       Brain;
+// A global instance of vex::competition
+vex::competition Competition;
+
+// define your global instances of motors and other devices here
+
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the cortex has been powered on and    */ 
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton( void ) {
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+  
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous( void ) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol( void ) {
+  // User control code here, inside the loop
+  while (1) {
+  
+    vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+    //Set up callbacks for autonomous and driver control periods.
+    Competition.autonomous( autonomous );
+    Competition.drivercontrol( usercontrol );
+    
+    //Run the pre-autonomous function. 
+    pre_auton();
+       
+    //Prevent main from exiting with an infinite loop.                        
+    while(1) {
+      vex::task::sleep(100);//Sleep the task for a short amount of time to prevent wasted resources.
+    }    
+       
+}
