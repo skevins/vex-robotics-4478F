ee93f5eba9d8c03e3f40bbd53d6f365b91612b77
Aiden P <50061154+PrecurseAP@users.noreply.github.com>
2019-09-16 19:21:07 -0400

    Declare motors, begin drive code

--- F-Team_main/src/main.cpp
+++ F-Team_main/src/main.cpp
@@ -10,70 +10,41 @@
 
 using namespace vex;
 
-vex::motor BackLeft = vex::motor()
-
 // A global instance of vex::brain used for printing to the V5 brain screen
 vex::brain       Brain;
 // A global instance of vex::competition
 vex::competition Competition;
 
-// define your global instances of motors and other devices here
+//Declaring motors
+vex::motor backLeft = vex::motor( vex::PORT1);
+vex::motor backRight = vex::motor( vex::PORT2, true);
+vex::motor frontLeft = vex::motor( vex::PORT3, true);
+vex::motor frontRight = vex::motor( vex::PORT4);
+vex::motor lift1 = vex::motor( vex::PORT5);
+vex::motor lift2 = vex::motor( vex::PORT6);   //SOME MOTORS MAY NEED REVERSING
+vex::motor clawGrabber = vex::motor( vex::PORT8);
+vex::motor clawAdjuster = vex::motor( vex::PORT8);
 
-
-/*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the cortex has been powered on and    */ 
-/*  not every time that the robot is disabled.                               */
-/*---------------------------------------------------------------------------*/
+//Create controller object
+vex::controller Controller1 = vex::controller();
 
 void pre_auton( void ) {
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
-  
-}
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
+}
 
 void autonomous( void ) {
-  // ..........................................................................
-  // Insert autonomous user code here.
-  // ..........................................................................
 
 }
 
-/*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
-/*---------------------------------------------------------------------------*/
-
 void usercontrol( void ) {
-  // User control code here, inside the loop
   while (1) {
-  
+    frontLeft.spin(directionType::fwd, Controller1.Axis3.value(), velocityUnits::pct);
+    frontRight.spin(directionType::fwd, Controller1.Axis2.value(), velocityUnits::pct);
+
     vex::task::sleep(20); //Sleep the task for a short amount of time to prevent wasted resources. 
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
 int main() {
     //Set up callbacks for autonomous and driver control periods.
     Competition.autonomous( autonomous );
